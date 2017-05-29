c616be5313b8a2e133333ec98370d37e071f3a68
Alec Rudd <alecrudd@Alecs-MacBook-Pro.local>
2017-05-29 17:11:09 -0400

    restructure

---
+++ main.py
@@ -0,0 +1,42 @@
+# main.py
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+from barcode import find_barcode
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+def gen_barcode(camera):
+    while True:
+        frame = camera.get_barcode()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/barcode/<variable>')
+def get_barcode(variable):
+    return Response(gen_barcode(VideoCamera(int(variable))),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/stream/<variable>')
+def stream_camera(variable):
+    print 'using camera: ' + variable
+    return Response(gen(VideoCamera(int(variable))),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
