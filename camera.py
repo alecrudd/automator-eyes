654166f0858815b3858200122f79f8603a3351f7
Alec Rudd <alec.b.rudd@gmail.com>
2017-05-31 10:18:20 -0400

    propery multithreading for retreiving camera images

--- camera.py
+++ camera.py
@@ -3,30 +3,39 @@ camera.py
 '''
 
 import cv2
+import sys
+import threading
 from barcode import find_barcode
 
 
 class VideoCamera(object):
     def __init__(self, num):
-        self.video = cv2.VideoCapture(num)
-        self.output = self.video.read()
-        self.grab_started = False
+        try:
+            self.video = cv2.VideoCapture(num)
+            self.output = self.video.read()[1]
+            self.grab_started = False
+        except:
+            pass
 
     def is_open(self):
-        return self.video.isOpen()
+        return self.video.isOpened()
 
     def __del__(self):
         self.video.release()
+
+    def grab_frames(self):
+        while self.grab_started:
+            success, image = self.video.read()
+            if success:
+                self.output = image.copy()
 
     def start_frame_grab(self):
         if(self.grab_started):
             return False
         print 'Starting the frame grabber'
         self.grab_started = True
-        while self.grab_started:
-            success, image = self.video.read()
-            if success:
-                self.output = image.copy()
+        grabber_thread = threading.Thread(target=self.grab_frames)
+        grabber_thread.start()
 
     def stop_frame_grab(self):
         self.grab_started = False
@@ -37,9 +46,11 @@ class VideoCamera(object):
             # We are using Motion JPEG, but OpenCV defaults to capture raw
             # images, so we must encode it into JPEG in order to correctly
             # display video stream.
+
             ret, jpeg = cv2.imencode('.jpg', self.output)
             return jpeg.tobytes()
         except:
+            print 'Error encoding frame:', sys.exc_info()[0]
             return None
 
     def get_barcode_frame(self):
--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 from flask import Flask, Response
 from camera import VideoCamera
 import socket
+import sys
+import argparse
+import time
 
 app = Flask(__name__)
 
@@ -22,6 +25,7 @@ def index():
 
 
 def gen(camera):
+    print 'Starting the normal stream'
     camera.start_frame_grab()
     while True:
         frame = camera.get_frame()
@@ -30,7 +34,7 @@ def gen(camera):
 
 
 def gen_barcode(camera):
-    print 'starting barcode feed'
+    print 'Starting barcode stream'
     camera.start_frame_grab()
     while True:
         frame = camera.get_barcode_frame()
@@ -38,31 +42,53 @@ def gen_barcode(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@app.route('/barcode/<camnum>')
-def stream_barcode(camnum):
+def add_camera(cam_name):
+    if(cam_name not in cameras):
+        print 'Camera doesn\'t exist, adding new camera ', cam_name
+        cam = VideoCamera(int(cam_name))
+        if(cam.is_open() is False):
+            return False, 'Camera %s failed to open' % cam_name
+        cameras[cam_name] = cam
+        return True, 'Opened camera'
+    return True, 'Cameras already opened'
+
+
+@app.route('/barcode/<cam_name>')
+def stream_barcode(cam_name):
     try:
-        if(camnum not in cameras):
-            cameras[camnum] = VideoCamera(camnum)
-            if(cameras[camnum].isOpen is False):
-                return 'Camera %d failed to open' % camnum
-        return Response(gen_barcode(cameras[int(camnum)]),
+        (success, message) = add_camera(cam_name)
+        if(success is False):
+            return message
+        return Response(gen_barcode(cameras[cam_name]),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
     except:
-        return 'Failed to retreive barcode stream from camera ' + camnum
+        print 'Failed to retreive bar stream from camera ', sys.exc_info()[0]
+
+        return 'Failed to retreive barcode stream from camera ', cam_name
 
 
-@app.route('/stream/<camnum>')
-def stream_camera(camnum):
+@app.route('/stream/<cam_name>')
+def stream_camera(cam_name):
     try:
-        if(camnum not in cameras):
-            cameras[camnum] = VideoCamera(camnum)
-            if(cameras[camnum].isOpen is False):
-                return 'Camera %d failed to open' % camnum
-        return Response(gen(cameras[int(camnum)]),
+        cam_num = int(cam_name)
+        (success, message) = add_camera(cam_num)
+        if(success is False):
+            return message
+        return Response(gen(cameras[int(cam_num)]),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
     except:
-        return 'Failed to retreive c stream from camera ' + camnum
+        print 'Failed to retreive c stream from camera. Excpetion: ', \
+                sys.exc_info()[0]
+        return 'Failed to retreive c stream from camera ', cam_num
 
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-L', '--use-local', action='store_true',
+                    help='use local host for camera')
+
+use_local = parser.parse_args(['--use-local'])
 
 if __name__ == '__main__':
-    app.run(host=ip, port=port, threaded=True)
+    if use_local:
+        host = 'localhost'
+    app.run(host=host, port=port, threaded=True)
