088344c654f6fd982902fd921de2a7c96eac2575
alecrudd <alec.b.rudd@gmail.com>
2017-06-04 08:04:33 -0400

    Update open-eyes.sh

--- open-eyes.sh
+++ open-eyes.sh
@@ -3,4 +3,4 @@
 uvcdynctrl -s 'Brightness' 0.1
 
 workon automator-eyes
-python /opt/automator-eyes/main.py --local --rotate -3 & > logs.txt
+python /opt/automator-eyes/main.py --local --rotate -3 > logs.txt &
